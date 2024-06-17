1081bfe971fe008bbe6211fb4e61a0f33778f02e
Thomas Dowling <thomasgdowling@gmail.com>
2024-06-17 11:27:26 +0100

    add content to forms_radio.html

--- js/functions.js
+++ js/functions.js
@@ -33,9 +33,63 @@ function draw(txt, x, y) {
     ctx.fillStyle = "white"
     ctx.fillText(txt, x, y);
 }
-function drawAlt(txt, x, y,id, color) {
+function drawAlt(txt, x, y, id, color) {
     const ctx = document.getElementById(id).getContext("2d");
     ctx.font = "24px Helvetica";
     ctx.fillStyle = color;
     ctx.fillText(txt, x, y);
 }
+
+function generateParams(...args) {
+    var tmp = "";
+    if (args.length < 3) { tmp += `${args[0]}=${args[1]}`; } else {
+        tmp += `${args[0]}=${args[1]}`;
+        for (let i = 2; i <
+            args.length; i += 2) { tmp += `&${args[i]}=${args[i + 1]}`; }
+    }
+    return tmp;
+}
+
+function addQueryStringToUrl(url, ...args) {
+    var tmp = url + '?';
+    if (args.length < 3) {
+        // console.log(args);
+        tmp = `${tmp}${args[0]}=${args[1]}`;
+    }
+    else {
+        tmp = `${tmp}${args[0]}=${args[1]}`;
+        for (let i = 2; i < args.length; i += 2) {
+            tmp += `&${args[i]}=${args[i + 1]}`;
+        }
+    }
+    return tmp;
+}
+
+function makePostRequest(url, params, fn) {
+    let oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", fn);
+    oReq.open('POST', url);
+    oReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // oReq.setRequestHeader("Content-length", params.length);
+    // oReq.setRequestHeader("Connection", "close");
+    oReq.send(params);
+}
+
+function makeRequest(url, fn) {
+    let oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", fn);
+    oReq.open('GET', url);
+    oReq.send();
+}
+
+  function checkField(event, in_id) {
+    event.preventDefault();
+    let tmp = document.getElementById(in_id);
+    if (tmp.value === '') {
+      var msgName = document.getElementsByClassName('msg_name')[0];
+      msgName.innerHTML = "!Required field";
+      msgName.style.backgroundColor = "red";
+      return false;
+    }
+    return true;
+  }
